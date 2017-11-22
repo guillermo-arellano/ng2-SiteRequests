f1cbb10f81f000b1d2e49c7e39b0b6fa4b9f1c0f
Guillermo Arellano <guillermo.arellanojr@gmail.com>
2017-11-21 21:05:23 -0600

    Adding Covalent and Materials modules to main app module definition.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { MaterialModule } from '@angular/material';
+import { MatButtonModule, MatListModule, MatIconModule, MatCardModule, MatMenuModule, MatInputModule, MatButtonToggleModule,
+  MatProgressSpinnerModule, MatSelectModule, MatSlideToggleModule, MatDialogModule, MatSnackBarModule, MatToolbarModule,
+  MatTabsModule, MatSidenavModule, MatTooltipModule, MatRippleModule, MatRadioModule, MatGridListModule,
+  MatDatepickerModule, MatNativeDateModule, MatSliderModule, MatAutocompleteModule } from '@angular/material';
 import 'hammerjs';
-import { CovalentCoreModule } from '@covalent/core';
+import { CovalentCommonModule, CovalentLayoutModule, CovalentMediaModule, CovalentExpansionPanelModule,
+  CovalentStepsModule, CovalentLoadingModule, CovalentDialogsModule, CovalentSearchModule, CovalentPagingModule,
+  CovalentNotificationsModule, CovalentMenuModule, CovalentDataTableModule, CovalentMessageModule } from '@covalent/core';
 
 import { AppComponent } from './app.component';
 
@@ -16,8 +21,44 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule.forRoot(),
-    CovalentCoreModule.forRoot()
+    /** Materials Modules*/
+    MatButtonModule, 
+    MatListModule, 
+    MatIconModule, 
+    MatCardModule, 
+    MatMenuModule, 
+    MatInputModule, 
+    MatButtonToggleModule,
+    MatProgressSpinnerModule,
+    MatSelectModule, 
+    MatSlideToggleModule, 
+    MatDialogModule, 
+    MatSnackBarModule, 
+    MatToolbarModule,
+    MatTabsModule, 
+    MatSidenavModule, 
+    MatTooltipModule, 
+    MatRippleModule, 
+    MatRadioModule, 
+    MatGridListModule,
+    MatDatepickerModule, 
+    MatNativeDateModule, 
+    MatSliderModule, 
+    MatAutocompleteModule,
+    /** Covalent Modules */
+    CovalentCommonModule,
+    CovalentLayoutModule, 
+    CovalentMediaModule, 
+    CovalentExpansionPanelModule,
+    CovalentStepsModule, 
+    CovalentLoadingModule, 
+    CovalentDialogsModule, 
+    CovalentSearchModule, 
+    CovalentPagingModule,
+    CovalentNotificationsModule, 
+    CovalentMenuModule, 
+    CovalentDataTableModule, 
+    CovalentMessageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
